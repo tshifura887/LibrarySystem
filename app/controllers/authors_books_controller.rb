e0fc236885e70775cfec3e164890e02f646d5ddc
Talifhani Tshifura <tshifura887@gmail.com>
2022-07-04 10:52:44 +0200

    converting params search do downcase

--- app/controllers/authors_books_controller.rb
+++ app/controllers/authors_books_controller.rb
@@ -4,10 +4,10 @@ class AuthorsBooksController < ApplicationController
   
     def results
       @books = Book.all
-      book_ids = Book.where(title: params[:search]).ids
+      book_ids = Book.where(["LOWER(title) LIKE ?", "%#{params[:search]}"]).ids
       @books = Book.where(id: book_ids)
 
-      author_ids = Author.where(name: params[:search]).ids
+      author_ids = Author.where(["LOWER(name) LIKE ?", "%#{params[:search]}"]).ids
       book_authors_ids = AuthorsBook.where(author_id: author_ids).pluck(:book_id)
       @book_authors = Book.where(id: book_authors_ids)
     end
